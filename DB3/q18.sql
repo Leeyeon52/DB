3ce3b3a8f915ac9d8dd36fc91fe1d1919e3f67a3
Leeyeon52 <dsy4907@gmail.com>
2025-04-14 10:25:11 +0900

    Update q18.sql

--- DB3/q18.sql
+++ DB3/q18.sql
@@ -28,3 +28,28 @@ INSERT INTO BOOK_STOCK VALUES
 (1002, 'Hello JAVA', 'KIM', 2),
 (1003, 'Hello PYTHON', 'MOON', 3),
 (1004, 'Hello SERVER', 'SONG', 0);
+
+
+SELECT
+    book_id,
+    book_name,
+    book_writer,
+    price
+FROM
+    BOOK
+WHERE
+    (book_name, book_writer) IN (
+        SELECT
+            book_name,
+            book_writer
+        FROM
+            BOOK_STOCK
+        WHERE
+            stock >= 1
+    )
+ORDER BY
+    book_id;
+
+
+DESC BOOK;
+DESC BOOK_STOCK;
